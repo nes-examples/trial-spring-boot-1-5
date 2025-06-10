1caa5ffff23ecdce5bc44f23bee2b309299cf3c9
Robert McNees <86265089+robertmcnees@users.noreply.github.com>
2025-06-10 07:38:29 -0400

    Initial commit of maven and gradle boot 1.5 code
    This commit adds two new folders for maven and gradle builds.
    The code in both folders is identical. Both build systems are
    supported to allow the user to choose the system they prefer.

---
+++ maven/src/main/java/com/herodevs/nes/trial/spring/config/SecurityConfig.java
@@ -0,0 +1,41 @@
+package com.herodevs.nes.trial.spring.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/helloPets").permitAll()
+                .antMatchers("/allPets", "/pets").hasRole("USER")
+                .anyRequest().denyAll()
+                .and()
+                .httpBasic();
+    }
+
+    @Bean
+    @Override
+    public UserDetailsService userDetailsService() {
+        UserDetails user = User.withUsername("user")
+                .password("password")
+                .roles("USER")
+                .build();
+        return new InMemoryUserDetailsManager(user);
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService());
+    }
+}
---
+++ maven/src/test/java/com/herodevs/nes/trial/spring/Boot2ExampleApplicationTests.java
@@ -0,0 +1,17 @@
+package com.herodevs.nes.trial.spring;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class Boot2ExampleApplicationTests {
+
+	public Boot2ExampleApplicationTests() {
+
+	}
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
